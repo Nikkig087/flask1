fdc603a40a8f9259ffe22a0087dae917098ba86d
Nicola Getsee <Nicola.getsee@gmail.com>
2024-07-11 18:30:00 +0000

    advanced routing using member.html page and run.py new app.route for the about page

--- run.py
+++ run.py
@@ -23,6 +23,18 @@ def about():
         data = json.load(json_data)
     return render_template("about.html",page_title="About",company=data) #new varible called company which will be sent to the html template which is equal to the json_data its loading from the json_data file
 
+
+@app.route('/about/<member_name>') #this will pass in our name to the view below
+def about_member(member_name):
+    member= {}
+    with open("data/company.json","r") as json_data:
+        data = json.load(json_data) # this converts the data inside into json
+        for obj in data:
+            if obj["url"]== member_name:
+                member=obj
+    return render_template("member.html", member=member) #this first member is the varible being passed through into our html file, second member is the member object we created here on line 29
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html",page_title="Contact")
@@ -30,6 +42,8 @@ def contact():
 @app.route("/careers")
 def careers():
     return render_template("careers.html",page_title="Careers")
+
+
 
 if __name__ == "__main__": # then run with following arguments
     app.run(
